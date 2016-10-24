7f955e139b15bf2518037c6691d4d1576d0bf78f
Hugo Grochau <hugo@grochau.com>
2016-10-24 09:29:23 -0200

    Use queue to update players

--- scripts/update.js
+++ scripts/update.js
@@ -1,42 +1,37 @@
 const got = require('got');
-const parallelLimit = require('async/parallelLimit');
+const queue = require('async/queue');
+const retry = require('async/retry');
 const querystring = require('querystring');
 
-
 const API = 'http://127.0.0.1:8080/api/v1/';
-const INTERVAL = 15; // minutes
+
 const SIMULTANEOUS_WORKERS = 2;
 
-
 const update = () => {
-  console.log('Starting full update...');
+  console.log('Starting full update at ' + new Date().toISOString() + '...');
 
   console.log('Pulling players...');
 
   got(API + 'player', { 'json': true })
     .then(response => {
       console.log('Pulled players...');
-      let players = response.body.data;
-      let tasks = [];
-      players.forEach(player => {
-        tasks.push(callback => {
-          console.log('Starting update for player: ' + player.attributes.name);
-          got(API + 'player/' + player.attributes.platform + '/' + player.id + '/update')
-            .then(response => {
-              console.log('Updated player: ' + player.attributes.name);
-              callback(null);
-            })
-            .catch(error => {
-              console.log('Error updating player ' + player.attributes.name + '\n' + error.response.body)
-            });
-        });
-      });
-
-      parallelLimit(tasks, SIMULTANEOUS_WORKERS, () => console.log('Update finished. \nWaiting ' + INTERVAL + ' minutes until the next update...'));
+      q.push(response.body.data, err => { if (err) console.log(err) });
     })
-    .catch(error => console.log('Error getting players ' + error));
+    .catch(err => console.log('Error fetching players ' + err));
 };
 
-update();
-setInterval(update, 1000 * 60 * INTERVAL);
+let q = queue((player, callback) => {
+  console.log('Starting update for player: ' + player.attributes.name);
+  got(API + 'player/' + player.attributes.platform + '/' + player.id + '/update')
+    .then(response => {
+      console.log('Updated player: ' + player.attributes.name);
+      callback();
+    })
+    .catch(err => {
+      callback('Error updating player ' + player.attributes.name + '\n' + err.response.body);
+    });
+}, SIMULTANEOUS_WORKERS);
 
+q.drain = update;
+
+update();
--- src/lib/rocket_league_tracker_api.js
+++ src/lib/rocket_league_tracker_api.js
@@ -5,23 +5,22 @@ import _ from 'lodash';
 
 const API_URL = 'https://20kiyaost7.execute-api.us-west-2.amazonaws.com/prod';
 
-const getHeader = apiKey => { return {'X-API-KEY': apiKey } };
-
-async function getStats(platform, id, apiKey) {
-  try {
+function getStats(platform, id, apiKey) {
+  return new Promise((resolve, reject) => {
     let queryStr = querystring.stringify({
       'platform': platform,
       'name': id
     });
-    let response = await got(API_URL + '?' + queryStr, {
+    got(API_URL + '?' + queryStr, {
       'method': 'GET',
       'json': true,
-      'headers': getHeader(apiKey),
-    });
-    return _.omit(response.body, 'tracking');
-  } catch (err) {
-    throw new Error(err.response.body);
-  }
+      'headers': { 'X-API-KEY': apiKey },
+    })
+      .then(response => {
+        resolve( _.omit(response.body, 'tracking'));
+      })
+      .catch(err => reject(err));
+  });
 };
 
 export { getStats };
