431edbbf8cf5a7e206916032c3d37af1bd90495c
Hugo Grochau <hugo@grochau.com>
2017-01-15 18:02:17 -0200

    Add API documentation and improve error messages

--- src/api/player.js
+++ src/api/player.js
@@ -1,90 +1,179 @@
 import { Router } from 'express';
 import Player from '../models/player';
 import Mapper from 'jsonapi-mapper'
-import { getPlatformId } from '../lib/util';
+import { getPlatformId, getRanksFromStats } from '../lib/util';
 import { getStats } from '../lib/rocket_league_tracker_api'
+import { errMsg, okMsg } from '../lib/util';
 
 const mapper = new Mapper.Bookshelf();
 const format = (obj) =>  mapper.map(obj, 'player', {'enableLinks': false});
 
-const playlistMap = {
-  'Ranked Duel 1v1': '1v1',
-  'Ranked Doubles 2v2': '2v2',
-  'Ranked Solo Standard 3v3': '3v3s',
-  'Ranked Standard 3v3': '3v3'
-};
+/**
+ * @apiDefine PlayerNotFoundError
+ *
+ * @apiError PlayerNotFound The id of the Player was not found.
+ *
+ * @apiErrorExample PlayerNotFound Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": {
+ *         "message": "Player not found"
+ *       }
+ *     }
+ */
 
-const rankMap = {
-  'Grand Champion': 15,
-  'Super Champion': 14,
-  'Champion': 13,
-  'Superstar': 12,
-  'All-Star': 11,
-  'Shooting Star': 10,
-  'Rising Star': 9,
-  'Challenger Elite': 8,
-  'Challenger III': 7,
-  'Challenger II': 6,
-  'Challenger I': 5,
-  'Prospect Elite': 4,
-  'Prospect III': 3,
-  'Prospect II': 2,
-  'Prospect I': 1,
-  'Unranked': 0
-};
+/**
+ * @apiDefine InvalidPlatformError
+ *
+ * @apiError InvalidPlatform The platform is invalid
+ *
+ * @apiErrorExample InvalidPlatform Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *       "error": {
+ *         "message": "Invalid platform"
+ *       }
+ *     }
+ */
 
-const divisionMap = {
-  'V': 5,
-  'IV': 4,
-  'III': 3,
-  'II': 2,
-  'I': 1
-};
+/**
+ * @apiDefine DatabaseError
+ *
+ * @apiError Database There was an error with the application database
+ *
+ *  @apiErrorExample Database Error-Response:
+ *      HTTP/1.1 500 Internal Server Error
+ *      {
+ *        "error": {
+ *          "message": "Database error message"
+ *        }
+ *      }
+ */
 
-const getRanksFromStats = stats => {
-  let ranks = {};
-  stats.forEach(stat => {
-    let playlist = playlistMap[stat.label];
-    if (playlist) {
-      ranks[playlist] = stat.value;
-      // extract division and rank
-      let regex = /\[(\w{1,3})\]\s(.*)/;
-      let matched = regex.exec(stat.subLabel);
-      ranks[playlist+'_division'] = divisionMap[matched[1]];
-      ranks[playlist+'_tier'] = rankMap[matched[2]];
-    }
-  });
-  return ranks;
-};
-
+/**
+ * @apiDefine ExternalAPIError
+ *
+ * @apiError ExternalAPI There was an error fetching player data from the external API
+ *
+ * @apiErrorExample ExternalAPI Error-Response:
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+ *       "error": {
+ *         "message": "Error fetching player from external API"
+ *       }
+ *     }
+ */
 
 export default ({ config, bs }) => {
   const api = Router();
 
+  /**
+   * @api {get} /player/ Get all Players
+   * @apiName GetPlayers
+   * @apiGroup Player
+   *
+   * @apiSuccess {Object[]} data List of Players
+   *
+   * @apiUse DatabaseError
+   */
   api.get('/', (req, res) => {
     Player(bs)
       .fetchAll()
       .then(model => res.status(200).json(format(model)))
-      .catch(err => res.status(500).json({'error': {'message': 'Error fetching players'}}));
+      .catch(err => errMsg(res, 500, 'Database', err));
   });
 
+  /**
+   * @api {get} /player/:platform/:id Request Player information
+   * @apiName GetPlayer
+   * @apiGroup Player
+   *
+   * @apiParam {String="0","1","2","steam","ps4","xbox"} platform Player's platform
+   * @apiParam {String} id Player's unique ID.
+   *
+   * @apiSuccess {Object} data Player data
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "data": {
+   *         "type": "players",
+   *         "id": "76561198013819031",
+   *         "attributes": {
+   *           "platform": 0,
+   *           "1v1": 1373,
+   *           "1v1_games_played": 180,
+   *           "2v2": 1409,
+   *           "2v2_games_played": 564,
+   *           "3v3": 1150,
+   *           "3v3_games_played": 520,
+   *           "3v3s": 1110,
+   *           "3v3s_games_played": 67,
+   *           "last_update": "2017-01-15T19:59:29.858Z",
+   *           "1v1_tier": 15,
+   *           "2v2_tier": 15,
+   *           "3v3_tier": 15,
+   *           "3v3s_tier": 15,
+   *           "1v1_division": 1,
+   *           "2v2_division": 1,
+   *           "3v3_division": 1,
+   *           "3v3s_division": 3,
+   *           "name": "bd | Freedom",
+   *           "created_at": null
+   *         }
+   *       }
+   *     }
+   *
+   *
+   * @apiUse PlayerNotFoundError
+   *
+   * @apiUse InvalidPlatformError
+   *
+   * @apiUse DatabaseError
+   */
   api.get('/:platform/:id/', (req, res) => {
     let platform = getPlatformId(req.params.platform);
     if (platform == -1) {
-      res.status(500).json({'error': {'message': 'Invalid platform'}});
+      errMsg(res, 400, 'InvalidPlatform', 'Invalid platform');
     }
-
-    Player(bs, { 'id': req.params.id, 'platform': platform })
+    Player(bs, { 'id': req.params.id.toLowerCase(), 'platform': platform })
       .fetch({ 'require': true })
       .then(model => res.status(200).json(format(model)))
-      .catch(err => res.status(500).json({'error': {'message': 'Error fetching player: ' + err}}));
+      .catch(bs.Model.NotFoundError, err => errMsg(res, 404, 'PlayerNotFound', 'Player not found'))
+      .catch(err => errMsg(res, 500, 'Database', err));
 
   });
 
+  /**
+   * @api {get} /player/:platform/:id/update Update Player information
+   * @apiName UpdatePlayer
+   * @apiGroup Player
+   *
+   * @apiParam {String="0","1","2","steam","ps4","xbox"} platform Player's platform
+   * @apiParam {String} id Player's unique ID.
+   *
+   * @apiSuccess {Object} success Success message
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        "success": {
+   *          "message": "Player updated"
+   *        }
+   *     }
+   *
+   * @apiUse ExternalAPIError
+   *
+   * @apiUse PlayerNotFoundError
+   *
+   * @apiUse InvalidPlatformError
+   *
+   * @apiUse DatabaseError
+   */
   api.get('/:platform/:id/update', (req, res) => {
     let platform = getPlatformId(req.params.platform);
     if (platform == -1) {
-      res.status(500).json({'error': {'message': 'Invalid platform'}});
+      errMsg(res, 400, 'InvalidPlatform', 'Invalid platform');
     }
 
     let rltPlatform = 3 - platform; // rocket league tracker platform conversion
@@ -99,17 +188,51 @@ export default ({ config, bs }) => {
           })
           .set(ranks)
           .save()
-          .then(success => res.status(200).json({'success': {'message': 'Player updated'}}))
-          .catch(err => res.status(500).json({'error': {'message': 'Error updating player ' + err}}));
+          .then(success => okMsg('Player updated'))
+          .catch(bs.Model.NotFoundError, err => errMsg(res, 404, 'PlayerNotFound', 'Player not found'))
+          .catch(err => errMsg(res, 500, 'Database', err));
       })
-      .catch(err => res.status(500).json({'error': {'message': 'Error fetching player ', 'response': err}}));
+      .catch(err => errMsg(res, 500, 'ExternalAPI', 'Error fetching player from API'));
   });
 
-
+  /**
+   * @api {get} /player/:platform/:id/add Add Player to database
+   * @apiName AddPlayer
+   * @apiGroup Player
+   *
+   * @apiParam {String="0","1","2","steam","ps4","xbox"} platform Player's platform
+   * @apiParam {String} id Player's unique ID.
+   *
+   * @apiSuccess {Object} success Success message
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        "success": {
+   *          "message": "Player added"
+   *        }
+   *     }
+   *
+   * @apiError DuplicatePlayer The player is already added
+   *
+   * @apiErrorExample DuplicatePlayer Error-Response:
+   *     HTTP/1.1 409 Conflict
+   *     {
+   *       "error": {
+   *         "message": "Player already added"
+   *       }
+   *     }
+   *
+   * @apiUse ExternalAPIError
+   *
+   * @apiUse DatabaseError
+   *
+   * @apiUse InvalidPlatformError
+   */
   api.get('/:platform/:id/add', (req, res) => {
     let platform = getPlatformId(req.params.platform);
     if (platform == -1) {
-      res.status(500).json({'error': {'message': 'Invalid platform'}});
+      errMsg(res, 400, 'InvalidPlatform', 'Invalid platform');
     }
 
     let rltPlatform = 3 - platform; // rocket league tracker platform conversion
@@ -125,10 +248,48 @@ export default ({ config, bs }) => {
           });
         Player(bs)
           .save(attributes, {'method': 'insert'})
-          .then(success => res.status(200).json({'success': {'message': 'Player added'}}))
-          .catch(err => res.status(500).json({'error': {'message': err}}));
+          .then(success => okMsg('Player added'))
+          .catch(bs.Model.NoRowsUpdatedError, err => errMsg(res, 409, 'DuplicatePlayer', 'Player already added'))
+          .catch(err => errMsg(res, 500, 'Database', err));
       })
-      .catch(err => res.status(500).json({'error': {'message': err}}));
+      .catch(err => errMsg(res, 500, 'ExternalAPI', 'Error fetching player from API'));
+  });
+
+  /**
+   * @api {get} /player/:platform/:id/delete Delete Player from database
+   * @apiName DeletePlayer
+   * @apiGroup Player
+   *
+   * @apiParam {String="0","1","2","steam","ps4","xbox"} platform Player's platform
+   * @apiParam {String} id Player's unique ID.
+   *
+   * @apiSuccess {Object} success Success message
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *        "success": {
+   *          "message": "Player deleted"
+   *        }
+   *     }
+   *
+   *
+   * @apiUse DatabaseError
+   *
+   * @apiUse InvalidPlatformError
+   */
+  api.get('/:platform/:id/delete', (req, res) => {
+    let platform = getPlatformId(req.params.platform);
+    if (platform == -1) {
+      errMsg(res, 400, 'InvalidPlatform', 'Invalid platform');
+    }
+
+    Player(bs, { 'id': req.params.id, 'platform': platform })
+      .fetch({ 'require': true })
+      .destroy()
+      .then(success => okMsg('Player deleted'))
+      .catch(bs.Model.NotFoundError, err => errMsg(res, 404, 'PlayerNotFound', 'Player not found'))
+      .catch(err => errMsg(res, 500, 'Database', err));
   });
 
   return api;
--- src/lib/util.js
+++ src/lib/util.js
@@ -8,18 +8,70 @@
  *			collection.find({}, toRes(res));
  *		}
  */
-export function toRes(res, status=200) {
-	return (err, thing) => {
-		if (err) return res.status(500).send(err);
 
-		if (thing && typeof thing.toObject==='function') {
-			thing = thing.toObject();
-		}
-		res.status(status).json(thing);
-	};
-}
 
-export function getPlatformId(platformString) {
+const playlistMap = {
+  'Ranked Duel 1v1': '1v1',
+    'Ranked Doubles 2v2': '2v2',
+    'Ranked Solo Standard 3v3': '3v3s',
+    'Ranked Standard 3v3': '3v3'
+};
+
+const rankMap = {
+  'Grand Champion': 15,
+    'Super Champion': 14,
+    'Champion': 13,
+    'Superstar': 12,
+    'All-Star': 11,
+    'Shooting Star': 10,
+    'Rising Star': 9,
+    'Challenger Elite': 8,
+    'Challenger III': 7,
+    'Challenger II': 6,
+    'Challenger I': 5,
+    'Prospect Elite': 4,
+    'Prospect III': 3,
+    'Prospect II': 2,
+    'Prospect I': 1,
+    'Unranked': 0
+};
+
+const divisionMap = {
+  'V': 5,
+    'IV': 4,
+    'III': 3,
+    'II': 2,
+    'I': 1
+};
+
+const getRanksFromStats = stats => {
+  let ranks = {};
+  stats.forEach(stat => {
+    let playlist = playlistMap[stat.label];
+    if (playlist) {
+      ranks[playlist] = stat.value;
+      // extract division and rank
+      let regex = /\[(\w{1,3})\]\s(.*)/;
+      let matched = regex.exec(stat.subLabel);
+      ranks[playlist + '_division'] = divisionMap[matched[1]];
+      ranks[playlist + '_tier'] = rankMap[matched[2]];
+    }
+  });
+  return ranks;
+};
+
+const toRes = (res, status = 200) => {
+  return (err, thing) => {
+    if (err) return res.status(500).send(err);
+
+    if (thing && typeof thing.toObject==='function') {
+      thing = thing.toObject();
+    }
+    res.status(status).json(thing);
+  };
+};
+
+const getPlatformId = (platformString) => {
   switch (platformString.toLowerCase()) {
     case '0':
     case 'steam': return 0;
@@ -29,4 +81,25 @@ export function getPlatformId(platformString) {
     case 'xbox': return 2;
   }
   return -1;
-}
+};
+
+const errMsg = (res, status, code, message) => {
+  let jsonMessage = {
+    'error': {
+      'code': code,
+      'message': message
+    }
+  };
+  return res.status(status).json(jsonMessage);
+};
+
+const okMsg = (message) => {
+  let jsonMessage = {
+    'success': {
+      'message': 'Player added'
+    }
+  };
+  return res.status(200).json(jsonMessage);
+};
+
+export { toRes, getPlatformId, getRanksFromStats, errMsg, okMsg};
