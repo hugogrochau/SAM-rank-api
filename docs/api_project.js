5d0a218ed9e2ae47b6cf6bdc13eb11a127784647
Hugo Grochau <hugo@grochau.com>
2017-01-26 01:39:56 -0200

    Add docs to github

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "rocketleaguesam-api",
+  "version": "0.0.2",
+  "description": "Rocket League South America API",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-01-26T03:39:43.941Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.3"
+  }
+});
