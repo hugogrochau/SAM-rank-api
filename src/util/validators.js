3d1d41baa3c113b7ea7c10c32e73e34709d28e84
Hugo Grochau <hugo@grochau.com>
2017-02-11 17:05:50 -0200

    Minor refactoring

--- src/util/validators.js
+++ src/util/validators.js
@@ -3,10 +3,11 @@ import Player from '../models/player'
 
 const isValidIdForPlatform = (input, platform) => {
   const platformId = Player.getPlatformIdFromString(platform)
-  return platformId !== -1 &&
-  ((platformId === 0 && isValidSteamId(input)) ||
-  (platformId === 1 && isValidPSNId(input)) ||
-  (platformId === 2 && isValidXboxId(input)))
+  return platformId !== -1 && (
+    (platformId === 0 && isValidSteamId(input)) ||
+    (platformId === 1 && isValidPSNId(input)) ||
+    (platformId === 2 && isValidXboxId(input))
+  )
 }
 
 const isValidPlatform = (input) => Player.getPlatformIdFromString(input) !== -1
@@ -14,14 +15,13 @@ const isValidPlatform = (input) => Player.getPlatformIdFromString(input) !== -1
 const isValidSteamId = (input) => validator.isNumeric(input) && input.length === 17
 
 const isValidPSNId = (input) =>
-validator.isAlpha(input[0])
-&& input.length >= 3 && input.length <= 16
-&& validator.isAlphanumeric(validator.blacklist(input, '_-'))
+  validator.isAlpha(input[0])
+  && input.length >= 3 && input.length <= 16
+  && validator.isAlphanumeric(validator.blacklist(input, '_-'))
 
 const isValidXboxId = (input) =>
-validator.isAlphanumeric(input.trim()) && input.length >= 3 && input.length <= 15
+  validator.isAlphanumeric(input.trim()) && input.length >= 3 && input.length <= 15
 
-const isValidName = (input) =>
-validator.isLength(input, { min: 2, max: 32 })
+const isValidName = (input) => validator.isLength(input, { min: 2, max: 32 })
 
 export default { isValidIdForPlatform, isValidPlatform, isValidSteamId, isValidXboxId, isValidPSNId, isValidName }
